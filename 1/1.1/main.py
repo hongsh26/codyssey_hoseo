65cc3c9c8efc5977ca10cea2cb572ad2594b3f80
hongsh26 <swatch6264@gmail.com>
2025-03-10 18:12:38 +0900

    25.03.10 역방향,마크다운,에러문서 추가 완료

--- 1/1.1/main.py
+++ 1/1.1/main.py
@@ -1,11 +1,18 @@
-import csv
-
 print('Hello mars')
 
 try:
-    f = open('/Users/hongseunghyuk/PycharmProjects/1/42hoseo/1/1.1/mission_computer_main.log', 'r')
-    print("정방향")
-    for i in f:
+    f = open('1/1.1/mission_computer_main.log', 'r')
+    contents = f.readlines()
+    print('정방향')
+    for i in contents:
         print(i, end="")
-except FileNotFoundError:
-    print("file is not exist")
+    print('역방향')
+    for j in contents[::-1]:
+        print(j, end="")
+    f.close()
+except FileNotFoundError as e:
+    print('file is not exist')
+    f1 = open('1/42hoseo/1/1.1/error.txt', 'w')
+    f1.write(str(e))
+    f1.close()
+    
